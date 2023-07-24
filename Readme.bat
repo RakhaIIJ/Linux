f932e34ef0856d39c22808e80e710d054859745f
RakhaIIJ <105347426+RakhaIIJ@users.noreply.github.com>
2023-07-24 21:18:01 +0700

    Update Readme.bat

--- Readme.bat
+++ Readme.bat
@@ -1,3 +1,12 @@
 shell.cloud.google.com
 chmod +x Linux.sh
 ./Linux.sh
+
+
+
+Note:
+Kali GPG error?
+ > gpg --keyserver keyserver.ubuntu.com --recv-key 7D8D0BF6
+ > gpg --fingerprint 7D8D0BF6
+ > gpg -a --export 7D8D0BF6 | apt-key add -
+ > apt update
