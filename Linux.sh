14254032a507a60aa1c462b857983e585213551e
RakhaIIJ <105347426+RakhaIIJ@users.noreply.github.com>
2023-07-24 20:48:31 +0700

    Create Linux.sh

---
+++ Linux.sh
@@ -0,0 +1,35 @@
+echo -e "$123\n$123\n" | sudo passwd
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+./ngrok tcp --region ap 3388 &>/dev/null &
+echo "======================="
+echo choose linux distro
+echo "======================="
+echo "kali - Kali Linux"
+echo "18.04 - Ubuntu 18.04"
+echo "16.04 - Ubuntu 16.04"
+echo "20.10 - Ubuntu 18.04"
+read -p "choose linux distro: " distro
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull danielguerra/ubuntu-xrdp
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : ubuntu"
+echo "Password : ubuntu"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Wait 1 minute to finish bot"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:$distro > /dev/null 2>&1
